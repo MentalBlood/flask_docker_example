0bdf5de02fa87e77af76c23a01ba93bfed5fa004
MentalBlood <neceporenkostepan@gmail.com>
2020-10-07 21:51:13 +0300

    reworked for dealing with POST application/json requests

--- app/main.py
+++ app/main.py
@@ -1,23 +1,38 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
+from logging.config import dictConfig
+
+dictConfig({
+    'version': 1,
+    'formatters': {'default': {
+        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+    }},
+    'handlers': {'wsgi': {
+        'class': 'logging.StreamHandler',
+        'formatter': 'default'
+    }},
+    'root': {
+        'level': 'INFO',
+        'handlers': ['wsgi']
+    }
+})
 
 app = Flask(__name__)
 api = Api(app)
 
-class Sum(Resource):
-	def get(self):
-		args = request.args
-		a = args['a']
-		b = args['b']
-		data = None
-		if not a.isnumeric():
-			data = 'a'
-		elif not b.isnumeric():
-			data = 'b'
-		else:
-			data = int(a) + int(b)
-		return {'data': data}
-api.add_resource(Sum, '/add')
+@app.route('/add', methods=['POST'])
+def json_example():
+	args = request.get_json()
+	a = args['a']
+	b = args['b']
+	data = None
+	if not type(a) == type(1):
+		data = 'a'
+	elif not type(b) == type(1):
+		data = 'b'
+	else:
+		data = int(a) + int(b)
+	return {'data': data}
 
 if __name__ == '__main__':
 	app.run()
